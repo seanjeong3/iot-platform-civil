b2b024e22850d654373798bc6deef547d7a55a05
seanjeong3 <seanjeong3@gmail.com>
2019-01-25 23:46:21 -0800

    create install_database.py

--- installation/install_database.py
+++ installation/install_database.py
@@ -133,7 +133,7 @@ def export_cassandra_path(param):
 			filedata += '\n'
 			filedata += 'export CQLSH_NO_BUNDLED=true\n'
 		if 'export PATH=\"{0}/bin:\$PATH\"'.format(param["cassandra_home"]) not in filedata:
-			filedata += 'export PATH=\"{0}/bin:\$PATH\"\n'.format(param["cassandra_home"])
+			filedata += 'export PATH={0}/bin:\$PATH\n'.format(param["cassandra_home"])
 			with open('{0}/.profile'.format(param["home_path"]), 'w') as f :
 				f.write(filedata)
 
