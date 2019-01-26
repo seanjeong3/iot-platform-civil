3e2414606904501045ef16b048006f01fdc0b884
seanjeong3 <seanjeong3@gmail.com>
2019-01-26 11:46:21 -0800

    update install_webserver.py

--- installation/install_webserver.py
+++ installation/install_webserver.py
@@ -3,18 +3,20 @@ import os
 
 def prepare_database_param(filepath):
 	param = json.loads(open(filepath).read())["ws_info"]
-	param["home_path"] = os.path.expanduser("~")
-	param["current_path"] = os.getcwd()
 	param["webserver_path"] = "{0}/webserver".format(os.getcwd())
 	return param
 
 
 # Install webserver dependency using shell commands
-def install_database_dependency():
+def install_webserver_dependency():
 	print("[install_webserver.py] sudo apt-get update")
 	os.system('sudo apt-get update')
 	print("[install_webserver.py] sudo apt install openjdk-8-jre-headless")
 	os.system('sudo apt install openjdk-8-jre-headless')
+
+
+# Install node.js and npm
+def install_nodejs():
 	print("[install_webserver.py] curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -")
 	os.system('curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -')
 	print("[install_webserver.py] sudo apt-get install -y nodejs")
@@ -25,12 +27,8 @@ def install_database_dependency():
 	os.system('sudo aptitude install npm')
 
 
-# # Download Cassandra (v3.9) and unzip it to the cassandra_path
-# def install_cassandra(param):
-# 	os.system("wget https://archive.apache.org/dist/cassandra/3.9/apache-cassandra-3.9-bin.tar.gz")
-# 	os.system('tar -zxvf apache-cassandra-3.9-bin.tar.gz -C {0}'.format(param["cassandra_path"]))
-# 	os.system("rm apache-cassandra-3.9-bin.tar.gz")
-# 	os.system('sudo chown -R $USER:$GROUP {0}'.format(param["cassandra_home"]))
+def install_npm(param):
+	os.system('npm install --prefix {0}'.format(param["webserver_path"]))
 
 
 def install_webserver(filepath):
@@ -39,14 +37,12 @@ def install_webserver(filepath):
 	param = prepare_database_param(filepath)
 	# Install dependencies
 	print("[install_webserver.py] Install webserver dependency.")
-	install_database_dependency()
-	# # Install Cassandra 
-	# print("[install_database.py] Install Cassandra.")
-	# install_cassandra(param)
-	# update_cassandra_config(param)
-	# export_cassandra_path(param)
-	# make_session_keep_alive()
-	# enable_cassandra_auto_auth(param)
+	install_webserver_dependency()
+	# Install node.js
+	print("[install_webserver.py] Install node.js.")
+	install_nodejs()
+	# Install npm on the webserver folder
+	intstall_npm()
 
 
 def uninstall_webserver(filepath):
