516ef1bd9eac5c4b577d5579ba321963a3ed44be
seanjeong3 <seanjeong3@gmail.com>
2019-01-25 16:55:04 -0800

    create install_database.py

--- installation/install_database.py
+++ installation/install_database.py
@@ -58,6 +58,7 @@ def create_database_folders(param):
 	os.system('sudo chown -R $USER:$GROUP {0}'.format(param["saved_caches_directory"]))
 
 
+# Install database dependency using shell commands
 def install_database_dependency():
 	print("[install_database.py] sudo apt-get update")
 	os.system('sudo apt-get update')
@@ -69,12 +70,73 @@ def install_database_dependency():
 	os.system('pip install cassandra-driver')
 
 
+# Download Cassandra (v3.9) and unzip it to the cassandra_path
 def install_cassandra(param):
 	os.system("wget https://archive.apache.org/dist/cassandra/3.9/apache-cassandra-3.9-bin.tar.gz")
 	os.system('tar -zxvf apache-cassandra-3.9-bin.tar.gz -C {0}'.format(param["cassandra_path"]))
 	os.system("rm apache-cassandra-3.9-bin.tar.gz")
 	os.system('sudo chown -R $USER:$GROUP {0}'.format(param["cassandra_home"]))
 
+
+# Update Cassandra configuration according to setup.json
+def update_cassandra_config(param):
+	# Update cassandra.yaml
+	with open('{0}/conf/cassandra.yaml'.format(param["cassandra_home"]), 'r') as f :
+		filedata = f.read()
+		filedata = filedata.replace('cluster_name: \'Test Cluster\'', 'cluster_name: \'{0}\''.format(param["cluster_name"]))
+		filedata = filedata.replace('- seeds: "127.0.0.1"', '- seeds: \"{0}\"'.format(", ".join([s for s in param["seeds"]])))
+		filedata = filedata.replace('# hints_directory: /var/lib/cassandra/hints', 'hints_directory: {0}'.format(param["hints_directory"]))
+		filedata = filedata.replace('# data_file_directories:', 'data_file_directories:')
+		filedata = filedata.replace('#     - /var/lib/cassandra/data', '     - {0}'.format(param["data_file_directories"]))
+		filedata = filedata.replace('# commitlog_directory: /var/lib/cassandra/commitlog', 'commitlog_directory: {0}'.format(param["commitlog_directory"]))
+		filedata = filedata.replace('# saved_caches_directory: /var/lib/cassandra/saved_caches', 'saved_caches_directory: {0}'.format(param["saved_caches_directory"]))
+		filedata = filedata.replace('listen_address: localhost', 'listen_address: {0}'.format(param["listen_address"]))
+		filedata = filedata.replace('# broadcast_address: 1.2.3.4', 'broadcast_address: {0}'.format(param["broadcast_address"])) # param["ip_address"]
+		filedata = filedata.replace('rpc_address: localhost', 'rpc_address: {0}'.format(param["rpc_address"]))
+		filedata = filedata.replace('# broadcast_rpc_address: 1.2.3.4', 'broadcast_rpc_address: {0}'.format(param["broadcast_rpc_address"])) # param["ip_address"]
+		filedata = filedata.replace('# rpc_min_threads: 16', 'rpc_min_threads: {0}'.format(param["rpc_min_threads"]))
+		filedata = filedata.replace('# rpc_max_threads: 2048', 'rpc_max_threads: {0}'.format(param["rpc_max_threads"]))
+		filedata = filedata.replace('read_request_timeout_in_ms: 5000', 'read_request_timeout_in_ms: {0}'.format(param["read_request_timeout_in_ms"]))
+		filedata = filedata.replace('range_request_timeout_in_ms: 10000', 'range_request_timeout_in_ms: {0}'.format(param["range_request_timeout_in_ms"]))
+		filedata = filedata.replace('write_request_timeout_in_ms: 2000', 'write_request_timeout_in_ms: {0}'.format(param["write_request_timeout_in_ms"]))
+		filedata = filedata.replace('counter_write_request_timeout_in_ms: 5000', 'counter_write_request_timeout_in_ms: {0}'.format(param["counter_write_request_timeout_in_ms"]))
+		filedata = filedata.replace('cas_contention_timeout_in_ms: 1000', 'cas_contention_timeout_in_ms: {0}'.format(param["cas_contention_timeout_in_ms"]))
+		filedata = filedata.replace('truncate_request_timeout_in_ms: 60000', 'truncate_request_timeout_in_ms: {0}'.format(param["truncate_request_timeout_in_ms"]))
+		filedata = filedata.replace('request_timeout_in_ms: 10000', 'request_timeout_in_ms: {0}'.format(param["request_timeout_in_ms"]))
+		filedata = filedata.replace('authenticator: AllowAllAuthenticator', 'authenticator: {0}'.format(param["authenticator"]))
+		filedata = filedata.replace('authorizer: AllowAllAuthorizer', 'authorizer: {0}'.format(param["authorizer"]))
+		filedata = filedata.replace('endpoint_snitch: SimpleSnitch', 'endpoint_snitch: {0}'.format(param["endpoint_snitch"]))
+		filedata = filedata.replace('batch_size_warn_threshold_in_kb: 5', 'batch_size_warn_threshold_in_kb: {0}'.format(param["batch_size_warn_threshold_in_kb"]))
+		filedata = filedata.replace('batch_size_fail_threshold_in_kb: 50', 'batch_size_fail_threshold_in_kb: {0}'.format(param["batch_size_warn_threshold_in_kb"]))
+		filedata += '\n'
+		filedata += 'auto_bootstrap: false'
+		with open('{0}/conf/cassandra.yaml'.format(param["cassandra_home"]), 'w') as f :
+			f.write(filedata)
+	# Update cassandra-rackdc.properties
+	with open('{0}/conf/cassandra-rackdc.properties'.format(param["cassandra_home"]), 'r') as f :
+		filedata = f.read()
+		filedata = filedata.replace('dc=dc1', 'dc={0}'.format(param["data_center_id"]))
+		filedata = filedata.replace('rack=rack1', 'rack={0}'.format(param["rack_id"]))
+
+
+def export_cassandra_path(param):
+	# Still need to manually source it!
+	os.system('echo  "export CQLSH_NO_BUNDLED=true" >> ~/.profile')
+	os.system('echo  "export PATH=\"{0}/bin:\$PATH\"" >> ~/.profile'.format(param["cassandra_home"]))
+
+
+def make_session_keep_alive():
+	os.system('sudo sysctl -w net.ipv4.tcp_keepalive_time=60 net.ipv4.tcp_keepalive_probes=3 net.ipv4.tcp_keepalive_intvl=10')
+
+
+# Enable auth (default id: cassandra, default pw: cassandra)
+def enable_cassandra_auto_auth(param):
+	if param["authenticator"] == "PasswordAuthenticator":
+		os.system('mkdir -p {0}/.cassandra'.format(param["home_path"]))
+		os.system('touch {0}/.cassandra/cqlshrc'.format(param["home_path"]))
+		os.system('echo "[authentication]" >> {0}/.cassandra/cqlshrc'.format(param["home_path"]))
+		os.system('echo "username = cassandra" >> {0}/.cassandra/cqlshrc'.format(param["home_path"]))
+		os.system('echo "password = cassandra" >> {0}/.cassandra/cqlshrc'.format(param["home_path"]))
 
 
 def install_database(filepath):
@@ -90,6 +152,11 @@ def install_database(filepath):
 	# Install Cassandra 
 	print("[install_database.py] Install Cassandra.")
 	install_cassandra(param)
+	update_cassandra_config(param)
+	export_cassandra_path(param)
+	make_session_keep_alive()
+	enable_cassandra_auto_auth(param)
+
 
 
 
