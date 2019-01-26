21ef0549bda9243d0b08d8e4d3f878a12ad0b22a
seanjeong3 <seanjeong3@gmail.com>
2019-01-26 12:37:48 -0800

    add setup message

--- setup_schema.py
+++ setup_schema.py
@@ -1,4 +1,6 @@
 import os
 
 # Using cqlsh command to import db schema
+print("Start importing Cassandra database schema from schema/schema_v1.cql")
 os.system('cqlsh -e "SOURCE \'schema/schema_v1.cql\'"')
+print("Data schema has been successfully imported")
